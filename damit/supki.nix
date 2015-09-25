3dd9ea38dd3dc98e0ba036f04f14ad62e2341986
Matvey Aksenov <matvey.aksenov@gmail.com>
2015-09-25 19:32:18 +0300

    Nothing works.

--- damit/supki.nix
+++ damit/supki.nix
@@ -1,7 +1,7 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc7101" }: let
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc7102" }: let
   inherit (nixpkgs) pkgs;
   ghc = pkgs.haskell.packages.${compiler}.ghcWithPackages(ps: [
-    ps.hdevtools ps.doctest ps.hspec-discover ps.envparse pkg
+    ps.hdevtools ps.doctest ps.hspec-discover ps.hlint ps.ghc-mod ps.envparse pkg
   ]);
   pkg = (import ./default.nix { inherit nixpkgs compiler; });
 in
